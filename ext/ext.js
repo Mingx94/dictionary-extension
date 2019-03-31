2fe16f06832afffedbe1dfa50dec2aaa3a3394f1
Michael Tsai <mingxcv@gmail.com>
2019-03-31 14:28:29 +0800

    Initialize

---
+++ ext/ext.js
@@ -0,0 +1,18 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    title: "Cambridge Learner's Dictionary",
+    id: 'main',
+    contexts: ['selection'],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener(function(item, tab) {
+  let url =
+    'https://dictionary.cambridge.org/dictionary/learner-english/' +
+    item.selectionText;
+  if (tab.index === -1) {
+    chrome.tabs.create({ url: url });
+  } else {
+    chrome.tabs.create({ url: url, index: tab.index + 1 });
+  }
+});
