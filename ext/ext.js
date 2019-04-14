8ac055617d12447cd8b33aa33bafa6670ecbf88c
Michael Tsai <mingxcv@gmail.com>
2019-04-14 09:49:49 +0800

    v0.2 Add English Dictionary search

--- ext/ext.js
+++ ext/ext.js
@@ -1,19 +1,30 @@
 chrome.runtime.onInstalled.addListener(function() {
-  console.log(123);
   chrome.contextMenus.create({
-    title: "Cambridge Learner's Dictionary",
-    id: 'main',
+    title: `Learner's Dictionary`,
+    id: 'Learner',
+    contexts: ['selection'],
+  });
+  chrome.contextMenus.create({
+    title: `English Dictionary`,
+    id: 'En2En',
     contexts: ['selection'],
   });
 });
 
 chrome.contextMenus.onClicked.addListener(function(item, tab) {
-  let url =
-    'https://dictionary.cambridge.org/dictionary/learner-english/' +
-    item.selectionText;
-  if (tab.index === -1) {
-    chrome.tabs.create({ url: url });
+  let urlPrefix;
+  const { selectionText, menuItemId } = item;
+  if (menuItemId === 'Learner') {
+    urlPrefix = 'https://dictionary.cambridge.org/dictionary/learner-english/';
   } else {
-    chrome.tabs.create({ url: url, index: tab.index + 1 });
+    urlPrefix = 'https://dictionary.cambridge.org/dictionary/english/';
+  }
+  if (tab.index === -1) {
+    chrome.tabs.create({ url: urlPrefix + selectionText });
+  } else {
+    chrome.tabs.create({
+      url: urlPrefix + selectionText,
+      index: tab.index + 1,
+    });
   }
 });
